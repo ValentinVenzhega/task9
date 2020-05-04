63d5a60dd2caa14075e52cfbaff54043dcdc81f7
Valentin <valek917@yandex.ru>
2020-05-04 17:32:22 +0300

    task16

--- js/script.js
+++ js/script.js
@@ -231,4 +231,48 @@ window.addEventListener('DOMContentLoaded', function() {
          }
       }
    });
+
+   // calculator
+
+   let persons = document.querySelectorAll('.counter-block-input')[0],
+       restDay = document.querySelectorAll('.counter-block-input')[1],
+       place = document.getElementById('select'),
+       totalValue = document.getElementById('total'),
+       personSum = 0,
+       daysSum = 0,
+       total = 0;
+
+       totalValue.innerHTML = 0;
+
+       persons.addEventListener('change', function() {
+         personSum = +this.value;
+         total =(daysSum + personSum)*4000;
+
+         if (restDay.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+         } else {
+            totalValue.innerHTML = total;
+         }
+       });
+
+       restDay.addEventListener('change', function() {
+         daysSum = +this.value;
+         total =(daysSum + personSum)*4000;
+
+         if (persons.value == '' || restDay.value == '') {
+            totalValue.innerHTML = 0;
+         } else {
+            totalValue.innerHTML = total;
+         }
+       });
+
+       place.addEventListener('change', function() {
+         if (restDay.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+         } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value; 
+         }
+       });
+
 });
